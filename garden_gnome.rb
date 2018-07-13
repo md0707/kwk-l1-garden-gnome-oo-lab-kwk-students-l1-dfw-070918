66517fc62daa206f6867f417897ffc4cc0a40429
md0707 <ppuggy1@gmail.com>
2018-07-13 18:55:39 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,4 +1,11 @@
 class GardenGnome
-  attr_reader :name, :personality, :hat_color
+  attr_reader :name, :personality, :hat_color, :age
   attr_writer :age, :hat_color
-def initialize(name, age, personality, hat_color,)
+def initialize(name, age, personality, hat_color)
+  @name = name
+  @age = age
+  @personality = personality
+  @hat_color = hat_color
+end
+
+

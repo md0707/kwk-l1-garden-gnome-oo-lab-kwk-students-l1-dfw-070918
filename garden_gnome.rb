eca4b8d3e37b4333c194051a4743851228adbbc8
md0707 <ppuggy1@gmail.com>
2018-07-13 19:06:08 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,15 +1,15 @@
 class GardenGnome
   attr_reader :name, :personality, :hat_color, :age, :gluten_allergy
   attr_writer :name, :age, :hat_color, :gluten_allergy
-def initialize(name, age, personality = "evil", hat_color = "red", gluten_allergy)
+ def initialize(name, age, personality = "evil", hat_color = "red", gluten_allergy)
   @name = name
   @age = age
   @personality = personality
   @hat_color = hat_color
   @gluten_allergy = gluten_allergy
-end
+ end
 
-def gnaw
+ def gnaw
   puts "Gnawing on a tree!!!"
 end
 
@@ -19,6 +19,7 @@ end
 
 def introduce_self(name, age)
   puts "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
-  
+end
+
 
 

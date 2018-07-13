191f6627321393bc5471fafcc3f263d9fcbaeeff
md0707 <ppuggy1@gmail.com>
2018-07-13 18:57:19 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,11 +1,12 @@
 class GardenGnome
-  attr_reader :name, :personality, :hat_color, :age
-  attr_writer :age, :hat_color
-def initialize(name, age, personality, hat_color)
+  attr_reader :name, :personality, :hat_color, :age, :gluten_allergy
+  attr_writer :age, :hat_color, :gluten_allergy
+def initialize(name, age, personality, hat_color, gluten_allergy)
   @name = name
   @age = age
   @personality = personality
   @hat_color = hat_color
+  @gluten_allergy = gluten_allergy
 end
 
 
